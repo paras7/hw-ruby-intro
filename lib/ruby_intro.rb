14e8e5039f833e4d5af5039100dc60b05ae7b4d2
EC2 Default User <ec2-user@ip-172-31-20-28.us-east-2.compute.internal>
2019-09-19 00:15:40 +0000

    Done with HW1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,32 +4,81 @@
 
 def sum arr
   # YOUR CODE HERE
+  sum = 0
+  if arr.empty?
+    return 0
+  elsif !arr.empty?
+    arr.each { |i| sum+=i }
+  return sum
+  end
 end
+  
 
 def max_2_sum arr
   # YOUR CODE HERE
+  sum = 0
+  if arr.empty?
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  else
+    sum = arr.sort.last(2).sum
+  return sum
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  # YOUR CODE HERE.
+  if arr.empty?
+    return false
+  elsif !arr.empty?
+    arr.combination(2).any? {|x, y| x + y == n }
+  end
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  if (s[0] =~ /^[b-df-hj-np-tv-z]+/i)
+    return true
+  else 
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if s == "0"
+    return true
+  elsif (s =~ /^[0-1]+$/ && s.to_i % 4 == 0)
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  # YOUR CODE HERE
+  attr_accessor :isbn
+	attr_accessor :price
+	
+	def initialize(isbn, price)
+	  if isbn.empty? || price <= 0
+	    raise ArgumentError
+	  end
+	  @isbn = isbn
+	  @price = price
+	end
+	
+	def price_as_string
+	  sprintf("$%.2f", @price)
+	end
 end
